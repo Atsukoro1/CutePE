ff1a1885ce7f51dffa300583e8c3517520ca3a8a
Atsukoro1 <dornicakkuba@gmail.com>
2026-04-22 17:52:51 +0200

    yeah donkey, this is my swamp

--- CutePE/PEParser.cpp
+++ CutePE/PEParser.cpp
@@ -2,12 +2,25 @@
 
 PEParser::PEParser()
 {
+	this->raw_pe_data = nullptr;
+
 	this->pe_dos_header = nullptr;
 	this->pe_nt_headers = nullptr;
-	this->raw_pe_data = nullptr;
+
+	this->is_64bit = false;
+
+	this->export_directory = {};
+	this->import_directory = {};
+	this->resource_directory = {};
+	this->exception_directory = {};
+	this->security_directory = {};
+	this->reloc_directory = {};
+	this->debug_directory = {};
+
 	this->pe_section_headers = nullptr;
 	this->pe_imports = {};
-	this->is_64bit = false;
+	this->pe_resource = {};
+	this->pe_resource = nullptr;
 }
 
 DWORD PEParser::from_bytes(char* bytes)
@@ -69,18 +82,39 @@ PIMAGE_DATA_DIRECTORY PEParser::get_data_directory(DWORD type)
 	}
 }
 
-DWORD PEParser::rva_to_offset(DWORD rva) {
+DWORD PEParser::rva_to_offset(DWORD rva)
+{
 	PIMAGE_SECTION_HEADER section = this->pe_section_headers;
 	WORD num_sections = this->pe_nt_headers->FileHeader.NumberOfSections;
 
 	for (WORD i = 0; i < num_sections; i++) {
 		if (rva >= section[i].VirtualAddress && rva < (section[i].VirtualAddress + section[i].Misc.VirtualSize)) {
-
 			return (rva - section[i].VirtualAddress) + section[i].PointerToRawData;
 		}
 	}
 
 	return 0;
+}
+
+template <typename T> T* PEParser::rva_to_ptr(DWORD rva)
+{
+	DWORD offset = this->rva_to_offset(rva);
+	T* ptr = (T*)(this->raw_pe_data + offset);
+
+	return ptr;
+}
+
+DWORD PEParser::resolve_directories()
+{
+	this->export_directory = this->get_data_directory(IMAGE_DIRECTORY_ENTRY_EXPORT);
+	this->import_directory = this->get_data_directory(IMAGE_DIRECTORY_ENTRY_IMPORT);
+	this->resource_directory = this->get_data_directory(IMAGE_DIRECTORY_ENTRY_RESOURCE);
+	this->exception_directory = this->get_data_directory(IMAGE_DIRECTORY_ENTRY_EXCEPTION);
+	this->security_directory = this->get_data_directory(IMAGE_DIRECTORY_ENTRY_SECURITY);
+	this->reloc_directory = this->get_data_directory(IMAGE_DIRECTORY_ENTRY_BASERELOC);
+	this->debug_directory = this->get_data_directory(IMAGE_DIRECTORY_ENTRY_DEBUG);
+
+	return PE_FILE_SUCCESS;
 }
 
 DWORD PEParser::get_thunk_data_addr(char* thunk_ptr, int index)
@@ -101,13 +135,11 @@ ImportedFunction PEParser::resolve_thunk_function(ULONGLONG func_ptr)
 	ULONGLONG ordinal_flag = this->is_64bit ? IMAGE_ORDINAL_FLAG64 : IMAGE_ORDINAL_FLAG32;
 
 	if (func_ptr & ordinal_flag) {
-		library.name = std::to_string(func_ptr & 0xFFFF);
+		library.name = std::to_string(IMAGE_ORDINAL(func_ptr));
 		library.is_ordinal = true;
 	}
 	else {
-		DWORD name_offset = this->rva_to_offset((DWORD)func_ptr);
-		PIMAGE_IMPORT_BY_NAME pName = (PIMAGE_IMPORT_BY_NAME)(this->raw_pe_data + name_offset);
-
+		PIMAGE_IMPORT_BY_NAME pName = this->rva_to_ptr<IMAGE_IMPORT_BY_NAME>((DWORD)func_ptr);
 		library.name = std::string((char*)pName->Name);
 		library.hint = pName->Hint;
 		library.address = (uintptr_t)func_ptr;
@@ -119,11 +151,8 @@ ImportedFunction PEParser::resolve_thunk_function(ULONGLONG func_ptr)
 
 DWORD PEParser::parse_import_entries()
 {
-	PIMAGE_DATA_DIRECTORY import_directory = this->get_data_directory(IMAGE_DIRECTORY_ENTRY_IMPORT);
-	if (!import_directory->VirtualAddress) return PE_FILE_SUCCESS;
-
-	DWORD import_directory_offset = this->rva_to_offset(import_directory->VirtualAddress);
-	IMAGE_IMPORT_DESCRIPTOR* import_entries = (IMAGE_IMPORT_DESCRIPTOR*)(this->raw_pe_data + import_directory_offset);
+	if (!this->import_directory->VirtualAddress) return PE_FILE_SUCCESS;
+	PIMAGE_IMPORT_DESCRIPTOR import_entries = this->rva_to_ptr<IMAGE_IMPORT_DESCRIPTOR>(import_directory->VirtualAddress);
 
 	for (int i = 0; import_entries[i].Name != 0; i++) {
 		ImportedLibrary new_library{};
@@ -140,21 +169,29 @@ DWORD PEParser::parse_import_entries()
 			ImportedFunction new_func = this->resolve_thunk_function(raw_thunk_value);
 			new_library.functions.push_back(new_func);
 		}
+
 		this->pe_imports.push_back(new_library);
 	}
-	
+
 	return PE_FILE_SUCCESS;
 }
 
 DWORD PEParser::parse_resource_entries()
 {
-	PIMAGE_DATA_DIRECTORY data_directory = this->get_data_directory(IMAGE_DIRECTORY_ENTRY_RESOURCE);
-	if (!data_directory->VirtualAddress) return PE_FILE_SUCCESS;
+	if (!this->resource_directory->VirtualAddress) return PE_FILE_SUCCESS;
 
-	DWORD resource_directory_offset = this->rva_to_offset(data_directory->VirtualAddress);
-	PIMAGE_RESOURCE_DIRECTORY resource_directory = (PIMAGE_RESOURCE_DIRECTORY)(this->raw_pe_data + resource_directory_offset);
+	PIMAGE_RESOURCE_DIRECTORY root_resource_directory = this->rva_to_ptr<IMAGE_RESOURCE_DIRECTORY>(this->resource_directory->VirtualAddress);
+	this->pe_resource = root_resource_directory;
+
+	// TODO: Later, too lazy sry
+	/*PIMAGE_RESOURCE_DIRECTORY_ENTRY resource_directory_entries = (PIMAGE_RESOURCE_DIRECTORY_ENTRY)(root_resource_directory + 1);*/
 
 	return PE_FILE_SUCCESS;
+}
+
+DWORD PEParser::parse_reloc_entries()
+{
+    return PE_FILE_SUCCESS;
 }
 
 DWORD PEParser::parse()
@@ -169,10 +206,13 @@ DWORD PEParser::parse()
 
 	this->is_64bit = this->pe_nt_headers->OptionalHeader.Magic == IMAGE_NT_OPTIONAL_HDR64_MAGIC;
 
+	this->resolve_directories();
+
 	this->pe_section_headers = IMAGE_FIRST_SECTION(this->pe_nt_headers);
+
 	this->parse_import_entries();
 	this->parse_resource_entries();
-
+	this->parse_reloc_entries();
 
 	return PE_FILE_SUCCESS;
 }
--- CutePE/PEParser.h
+++ CutePE/PEParser.h
@@ -6,6 +6,9 @@
 #include <phnt_windows.h>
 #include <phnt.h>
 
+///
+/// File parsing status codes
+/// 
 #define PE_FILE_SUCCESS 0x0
 #define PE_FILE_READ_ERROR 0x1
 #define PE_FILE_SIZE_GET_ERROR 0x2
@@ -13,6 +16,9 @@
 #define PE_FILE_INVALID_DOS_HEADER 0x4
 #define PE_FILE_INVALID_NT_HEADER 0x5
 
+///
+///	Section related structs 
+///
 struct ImportedFunction {
 	std::string name;
 	WORD hint;
@@ -25,6 +31,24 @@ struct ImportedLibrary {
 	std::vector<ImportedFunction> functions;
 };
 
+struct ResourceEntry {
+	WORD id;
+	bool is_directory;
+	std::string name;
+	DWORD data_ptr;
+	std::vector<ResourceEntry> entries;
+};
+
+struct RelocEntry {
+	DWORD rva;
+	WORD type;
+};
+
+struct RelocBlock {
+	DWORD page_rva;
+	std::vector<RelocEntry> entries;
+};
+
 class PEParser
 {
 public:	
@@ -33,20 +57,47 @@ public:
 	PIMAGE_NT_HEADERS pe_nt_headers;
 	PIMAGE_SECTION_HEADER pe_section_headers;
 	std::vector<ImportedLibrary> pe_imports;
+	PIMAGE_RESOURCE_DIRECTORY pe_resource;
+	ResourceEntry pe_resource_entries;
+	RelocBlock pe_relocation_blocks;
 
 	PEParser();
 
 	DWORD from_bytes(char* bytes);
 	DWORD from_disk(char* path);
 
+private:
+	///
+	/// Variables & functions related to data directories
+	///
+	PIMAGE_DATA_DIRECTORY export_directory;
+	PIMAGE_DATA_DIRECTORY import_directory;
+	PIMAGE_DATA_DIRECTORY resource_directory;
+	PIMAGE_DATA_DIRECTORY exception_directory;
+	PIMAGE_DATA_DIRECTORY security_directory;
+	PIMAGE_DATA_DIRECTORY reloc_directory;
+	PIMAGE_DATA_DIRECTORY debug_directory;
+
+	DWORD resolve_directories();
+
 protected:
 	char* raw_pe_data;
 
 	DWORD parse();
+
+	///
+	/// Functions specific to parsing section entries
+	///
 	DWORD parse_import_entries();
-	DWORD parse_resource_entries();
 	DWORD get_thunk_data_addr(char* thunk_ptr, int index);
 	ImportedFunction resolve_thunk_function(ULONGLONG func_ptr);
 	PIMAGE_DATA_DIRECTORY get_data_directory(DWORD type);
+	DWORD parse_resource_entries();
+	DWORD parse_reloc_entries();
+
+	///
+	/// General purpose helper functions
+	///
 	DWORD rva_to_offset(DWORD rva);
+	template <typename T> T* rva_to_ptr(DWORD rva);
 };
